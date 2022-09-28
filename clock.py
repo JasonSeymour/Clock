7a97c916807f92e0f52d1687934e779a4a9a5ff6
Jason Seymour <jaseymour@gmail.com>
2022-09-27 23:14:01 -0500

    First attempt at basic working version

--- clock.py
+++ clock.py
@@ -5,4 +5,22 @@ from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 
-print('Hello')
+class clockApp(App):
+    sw_started= False
+    sw_seconds = 0
+    def update_time(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+        minutes, seconds = divmod(self.sw_seconds, 60)
+        self.root.ids.stopwatch.text = (
+            '%02d:%02d.[size=40]%02d[/size]'%
+            (int(minutes), int(seconds),
+             int(seconds* 100 % 100))
+        )
+        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 0)
+
+if __name__ == '__main__':
+    Window.clearcolor = get_color_from_hex('#101216')
+    clockApp().run()
