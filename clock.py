61ee7f042454244854ba3b4174406a15f09b3369
Jason Seymour <jaseymour@gmail.com>
2022-09-27 22:51:04 -0500

    Initial check in

---
+++ clock.py
@@ -0,0 +1,8 @@
+from time import strftime
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+print('Hello')
